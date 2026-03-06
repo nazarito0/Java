a04d04a2766a981f686a905a1de02d5b673df459
nazarito0 <pkhome754@gmail.com>
2026-03-06 10:25:42 +0200

    lab_6

--- lab6/lab6.java
+++ lab6/lab6.java
@@ -60,7 +60,6 @@ class Lake implements Comparable<Lake> {
         island.showIslandInfo();
     }
 
-    // hashCode() і equals() для HashSet
     @Override
     public int hashCode() {
         return Objects.hash(name);
@@ -74,7 +73,6 @@ class Lake implements Comparable<Lake> {
         return Objects.equals(name, other.name);
     }
 
-    // Comparable для TreeSet / сортування
     @Override
     public int compareTo(Lake other) {
         return this.name.compareTo(other.name);
@@ -87,33 +85,28 @@ class Lake implements Comparable<Lake> {
 
     public static void main(String[] args) {
 
-        // 1. Створюємо невпорядкований список (ArrayList)
         List<Lake> lakeList = new ArrayList<>();
         lakeList.add(new Lake("Blue Lake"));
         lakeList.add(new Lake("Green Lake"));
         lakeList.add(new Lake("Red Lake"));
-        lakeList.add(new Lake("Blue Lake")); // дублікат
+        lakeList.add(new Lake("Blue Lake"));
         lakeList.add(new Lake("Yellow Lake"));
 
         System.out.println("Original ArrayList:");
         System.out.println(lakeList);
 
-        // 2. HashSet - унікальні елементи
         Set<Lake> lakeHashSet = new HashSet<>(lakeList);
         System.out.println("\nHashSet (unique elements):");
         System.out.println(lakeHashSet);
 
-        // 3. Сортування ArrayList
         Collections.sort(lakeList);
         System.out.println("\nSorted ArrayList:");
         System.out.println(lakeList);
 
-        // 4. TreeSet - унікальні та відсортовані
         Set<Lake> lakeTreeSet = new TreeSet<>(lakeList);
         System.out.println("\nTreeSet (unique & sorted):");
         System.out.println(lakeTreeSet);
 
-        // 5. TreeMap - ключ = name, значення = Lake
         Map<String, Lake> lakeTreeMap = new TreeMap<>();
         for (Lake lake : lakeList) {
             lakeTreeMap.put(lake.getName(), lake);
@@ -123,26 +116,22 @@ class Lake implements Comparable<Lake> {
             System.out.println(entry.getKey() + " => " + entry.getValue());
         }
 
-        // 6. LinkedList
         LinkedList<Lake> lakeLinkedList = new LinkedList<>(lakeList);
         System.out.println("\nLinkedList:");
         System.out.println(lakeLinkedList);
 
-        // 7. Queue (LinkedList)
         Queue<Lake> lakeQueue = new LinkedList<>(lakeList);
         System.out.println("\nQueue (FIFO):");
         while (!lakeQueue.isEmpty()) {
             System.out.println(lakeQueue.poll());
         }
 
-        // 8. PriorityQueue
         PriorityQueue<Lake> lakePriorityQueue = new PriorityQueue<>(lakeList);
         System.out.println("\nPriorityQueue (sorted by natural order):");
         while (!lakePriorityQueue.isEmpty()) {
             System.out.println(lakePriorityQueue.poll());
         }
 
-        // Додатково: приклад Fish
         Lake.Fish salmon = new Lake.Fish("Salmon");
         salmon.swim();
     }
